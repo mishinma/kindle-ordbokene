1d28a20a19319a55c837f73712e957eb7ac7bd0b
Mikhail Mishin <mikhail.mishin@oda.com>
2024-03-29 00:59:02 +0100

    Clean page

--- selenium_scrape.py
+++ selenium_scrape.py
@@ -9,9 +9,25 @@ from bs4 import BeautifulSoup
 
 cService = webdriver.ChromeService(executable_path='/usr/bin/chromedriver')
 driver = webdriver.Chrome(service = cService)
-driver.get('https://ordbokene.no/bm/19580')
+# url = 'https://ordbokene.no/bm/53143'  # sjønne
+url = 'https://ordbokene.no/bm/21740'  # gå
+driver.get(url)
 
 try:
+    # Use XPath to find the button by its text "Vis bøyning"
+    # and considering it might be inside multiple span elements within a button
+    button_xpath = "//button[contains(@class, 'show-inflection') and contains(@class, 'v-btn')]" \
+                   "/span[contains(@class, 'v-btn__content')]" \
+                   "/span[contains(text(), 'Vis bøyning')]"
+
+    # Wait until the button is clickable
+    button_element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, button_xpath))
+    )
+
+    # Click the button
+    button_element.click()
+
     # Wait until the element with class 'article' is present on the page
     article_element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, 'article'))
